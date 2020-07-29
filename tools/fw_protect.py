edd7379a27cf744736704dd9a952a4e84a8910dc
Ryan Zhang <ryan@itsryan.org>
2020-07-29 14:31:10 +0000

    code cleanup + colorsss

--- tools/fw_protect.py
+++ tools/fw_protect.py
@@ -32,21 +32,13 @@ def protect_firmware(infile, outfile, version, message):
     # Load firmware binary from infile
     with open(infile, 'rb') as fp:
         firmware = fp.read()
-        
-    print("Firmware Size")
-    print(len(firmware))
-        
+
     #Load in Keys
     with open('secret_build_output.txt', 'rb') as fp:
         secrets = fp.read()
 
     key = secrets[0:16]
     rsa_key = RSA.import_key(secrets[16:])
-    
-#     print("KEYS")
-    
-#     #print(key)
-#     print(rsa_key.size_in_bytes())
 
     # Create variable to hold message
     msg = message.encode() 
@@ -58,19 +50,11 @@ def protect_firmware(infile, outfile, version, message):
     chunks=list()
 
     for i in range(chunks_needed):
-        print(i)
         chunks.append(firmware[i * CHUNK_SIZE:(i + 1) * CHUNK_SIZE])
 
     #Add the remaining chunk from the firmware (that is not 1K bytes)
     if(CHUNK_SIZE * (chunks_needed) - len(firmware) != 0):
-        print("Extra")
         chunks.append(firmware[CHUNK_SIZE * (chunks_needed):])
-        
-    
-    for chunk in chunks:
-        print(len(chunk))
-
-#   key = get_random_bytes(16) #For Testing Purposes only
 
     #Encrypt each Chunk with AES
     final_output = b'' 
@@ -99,17 +83,8 @@ def protect_firmware(infile, outfile, version, message):
         h = SHA256.new(ciphertext + metadata)
         signature = pkcs1_15.new(rsa_key).sign(h)
 
-
-#        Things for testing :D
-#         print(metadata)
-#         print(ciphertext)
-#         print(len(ciphertext))
-#         print(tag)
-#         print(len(tag))
-
         #Add result to final output
         final_output += (metadata + aes_cipher.nonce + tag + signature + ciphertext)
-        print(len(final_output))
 
     # Add release message, with packet index -1
     aes_cipher = AES.new(key, AES.MODE_GCM)
@@ -122,51 +97,29 @@ def protect_firmware(infile, outfile, version, message):
         processed_plain = msg
     else:
         processed_plain = pad(msg, BLOCK_SIZE)
-    print(processed_plain)
+    # Log some information
+    print('> Output size:', len(final_output), 'bytes')
+    print('> Release message:', processed_plain)
     ciphertext, tag = aes_cipher.encrypt_and_digest(processed_plain)
     #Get RSA Signature
     h = SHA256.new(ciphertext)
     signature = pkcs1_15.new(rsa_key).sign(h)
     #Final output
     final_output += (metadata + aes_cipher.nonce + tag + signature + ciphertext)
-
-#   More Testing
-#     print(final_output)
-
+    
 #   Write firmware blob to outfile
     with open(outfile, 'wb') as outfile:
         outfile.write(final_output)
-
-
-#Verifies the tag and decrypts the ciphertext
-def decrypt(nonce_var, metadata, cipher_text, tag_var, key):
-    try:
-        cipher = AES.new(key, AES.MODE_GCM, nonce=nonce_var)
-        cipher.update(metadata)
-#       cipher.verify(tag_var)
-        plaintext = cipher.decrypt_and_verify(cipher_text, tag_var)
-        print(plaintext)
-    except ValueError:
-        print("AIYA")
-
-        
-#Verifies the RSA Signature
-def verify_rsa(ciphertext, key, signature):
-    try:
-        h = SHA256.new(ciphertext)
-        pkcs1_15.new(key).verify(h, signature)
-        print("yay")
-        return 1
-    except ValueError:
-        print("Fail. Wrong Value")
-        return 0 
-    
+       
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Firmware Update Tool')
+    parser = argparse.ArgumentParser(description='Firmware Protect Tool')
     parser.add_argument("--infile", help="Path to the firmware image to protect.", required=True)
     parser.add_argument("--outfile", help="Filename for the output firmware.", required=True)
     parser.add_argument("--version", help="Version number of this firmware.", required=True)
     parser.add_argument("--message", help="Release message for this firmware.", required=True)
     args = parser.parse_args()
-
+    
+    print('\n\x1b[92mWelcome to the C.I.A. Firmware Protect Tool!')
+    print('(C) 2020 Completely Insecure Alice\x1b[0m\n')
     protect_firmware(infile=args.infile, outfile=args.outfile, version=int(args.version), message=args.message)
+    print(f'\x1b[96m\nDone! File saved to {args.outfile}. GLHF!\x1b[0m')
